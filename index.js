66a6887f25a2e7d466a408b9d64bc618abf57861
Aswin Krishna <aswinkrishna2923@gmail.com>
2025-06-06 22:55:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-window.onload = function() {
-    const element = document.getElementById("main-text");
-    // Initially hide the element
-    element.style.display = "none"; 
+const words = ["am learning Web Development","am an ISTP","do some Python programming","am Iron Man"]
+let index=0;
 
-    // After 1 second, show the element and set its opacity
-    setTimeout(function() {
-        element.style.display = "block";
-        setTimeout(function() {
-            element.style.opacity = 1; // Fade-in effect
-        }, 10);
-    }, 1000);
-};
-
-function toggleTheme() {
-    let body = document.getElementById("body");
-    let text = document.getElementById("main-text");
-    let image = document.getElementById("mspaint-aswin");
-    let button = document.getElementById("themeButton");
-
-    if (button.innerText === "Light") {
-        body.style.backgroundColor = 'black';
-        text.style.color = 'white';
-        image.src = '/IMAGES/mspaint_aswin.png'; // Update with the correct path
-        button.innerText = 'Dark';
-    } else {
-        body.style.backgroundColor = 'rgb(169, 218, 218)';
-        text.style.color = 'rgb(0, 0, 87)';
-        image.src = '/IMAGES/mspaint_aswin.png'; // Update with the correct path
-        button.innerText = 'Light';
-    }
-}
-
-if (window.innerWidth < 768) {
-    window.location.href = "/Mobile/mobile.html"; // your mobile version
-  }
+setInterval(()=>{
+    index = (index+1)%words.length;
+    document.getElementById("changing-text").textContent = words[index];
+},3000);
